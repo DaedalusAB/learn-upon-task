cf988ff5f3a092f57e74daa5e90da7ef9e0abf16
DaedalusAB <aleksandar.s.budic@gmail.com>
2022-02-09 12:51:46 +0100

    Basic outline (nav, header, main)

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
     NavigationComponent,
+    HeaderComponent,
   ],
   exports: [
     NavigationComponent,
+    HeaderComponent,
   ],
   imports: [
     CommonModule,
