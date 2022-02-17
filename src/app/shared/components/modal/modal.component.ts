21bc15d007604617a847ac109c0a29695ac26bd4
DaedalusAB <aleksandar.s.budic@gmail.com>
2022-02-17 18:17:20 +0100

    Rework the modal (no more footer)

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,26 +1,16 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'lu-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent {
+  @Input() title: string | undefined;
   @Input() isShown: boolean = false;
   @Output() isShownChange: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  @Input() title: string | undefined;
-  @Input() primaryButtonText: string | undefined;
-  @Output() primaryAction: EventEmitter<void> = new EventEmitter<void>();
-
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  public emitPrimaryAction(): void {
-    this.primaryAction.emit();
-  }
 
   public close(): void {
     this.isShownChange.emit(false);
