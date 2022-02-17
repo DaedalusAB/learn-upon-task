f8ae1389863441752d553b695a2ca67177a71b5d
DaedalusAB <aleksandar.s.budic@gmail.com>
2022-02-17 18:28:32 +0100

    add global style for buttons

--- src/app/users/components/create-user/create-user.component.ts
+++ src/app/users/components/create-user/create-user.component.ts
@@ -1,31 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import {FormlyFieldConfig} from '@ngx-formly/core';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'lu-create-user',
   templateUrl: './create-user.component.html',
   styleUrls: ['./create-user.component.scss']
 })
-export class CreateUserComponent implements OnInit {
-  form = new FormGroup({});
-  model = { email: 'email@gmail.com' };
-  fields: FormlyFieldConfig[] = [
+export class CreateUserComponent {
+  public form = new FormGroup({});
+
+  public model: User = {} as User;
+
+  public fields: FormlyFieldConfig[] = [
     {
-      key: 'email',
+      key: 'email', 
       type: 'input',
       templateOptions: {
         label: 'Email address',
-        placeholder: 'Enter email',
+        placeholder: 'Users email',
         required: true,
       }
     }
   ];
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
   onSubmit(model: any) {
     console.log(model);
