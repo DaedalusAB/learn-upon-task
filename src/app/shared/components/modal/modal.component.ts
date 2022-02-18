d1b0b12c93c109b038458574fc10965f60d3e5de
DaedalusAB <aleksandar.s.budic@gmail.com>
2022-02-18 08:27:43 +0100

    Modal a11y improvements

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'lu-modal',
@@ -9,6 +9,13 @@ export class ModalComponent {
   @Input() title: string | undefined;
   @Input() isShown: boolean = false;
   @Output() isShownChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  //  This setter will focus the modal every time it opens, for A11Y compliance 
+  @ViewChild('modalBox') set modalBoxSetter(modalBox: ElementRef) {    
+    if(modalBox) {
+      setTimeout(() => modalBox.nativeElement.focus());
+    }
+  }
 
   constructor() { }
 
