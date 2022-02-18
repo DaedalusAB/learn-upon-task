6ee52016c5891d91199553735255ebdda392eb68
DaedalusAB <aleksandar.s.budic@gmail.com>
2022-02-18 09:36:06 +0100

    Spinner tests

--- src/app/core/components/spinner-overlay/spinner-overlay.component.spec.ts
+++ src/app/core/components/spinner-overlay/spinner-overlay.component.spec.ts
@@ -1,16 +1,17 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { SpinnerOverlayService } from '../../services/spinner-overlay.service';
 import { SpinnerOverlayComponent } from './spinner-overlay.component';
 
 describe('SpinnerOverlayComponent', () => {
+  let service: SpinnerOverlayService;
   let component: SpinnerOverlayComponent;
   let fixture: ComponentFixture<SpinnerOverlayComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SpinnerOverlayComponent ]
-    })
-    .compileComponents();
+      declarations: [SpinnerOverlayComponent],
+    }).compileComponents();
+    service = TestBed.inject(SpinnerOverlayService);
   });
 
   beforeEach(() => {
@@ -20,6 +21,41 @@ describe('SpinnerOverlayComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy();  
   });
+
+  it('should not be visible when created', () => {
+    fixture.detectChanges();
+    let spinnerImage = fixture.nativeElement.querySelector('img');
+    expect(component.isShown).toBeFalsy();
+    expect(spinnerImage).toBeFalsy();
+  });
+
+  it('should appear when service calls show() method', fakeAsync(() => {
+    fixture.detectChanges();
+    component.ngOnInit();
+    service.show();
+    fixture.detectChanges();
+    expect(component.isShown).toBeTruthy();
+  }));
+
+  it('should not appear when service calls hide() method', fakeAsync(() => {
+    component.isShown = true;
+    component.ngOnInit();
+    fixture.detectChanges();
+    
+    //  Confirm that its visible
+    let spinnerImage = fixture.nativeElement.querySelector('img');
+    expect(component.isShown).toBeTruthy();
+    expect(spinnerImage).toBeTruthy();
+
+    service.hide();
+    fixture.detectChanges();
+
+    //  Confirm that it was hidden
+    spinnerImage = fixture.nativeElement.querySelector('img');
+    expect(component.isShown).toBeFalsy();
+    expect(spinnerImage).toBeFalsy();
+  }));
+
 });
